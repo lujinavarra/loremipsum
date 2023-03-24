0cc0b82f2a524336964c4ce585606aada66920b8
Maria Lujan Navarra <lujinavarra@gmail.com>
2023-03-24 20:15:35 -0300

    tercer commit

---
+++ contar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Tiene: $(wc -l < loremipsum-1.txt) líneas"
+echo "Tiene: $(wc -l < loremipsum-2.txt) líneas"
+echo "Tiene: $(wc -l < loremipsum-3.txt) líneas"
+echo "Tiene: $(wc -l < loremipsum-4.txt) líneas"
+echo "Tiene: $(wc -l < loremipsum-5.txt) líneas"
